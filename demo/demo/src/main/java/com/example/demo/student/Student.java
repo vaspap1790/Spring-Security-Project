cebe7d94fb1b8f5abc343942dd2582233a545595
papadimitrakopoulosv <papadimitrakopoulosv@cbs.gr>
2019-12-24 11:34:31 +0200

    Basic Auth

---
+++ demo/demo/src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,23 @@
+package com.example.demo.student;
+
+public class Student {
+
+    private Integer studentId;
+    private String studentName;
+
+    public Student() {
+    }
+
+    public Student(Integer studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+}
