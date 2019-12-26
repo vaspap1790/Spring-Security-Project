08b7355f5d694ec6ca4067ed2b439af385d2b636
vaspap1790 <vaspap1790@gmail.com>
2019-12-26 11:25:45 +0200

    Form-based Authentication - Remember me feature

--- demo/demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ demo/demo/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -16,6 +16,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.example.demo.security.ApplicationUserRole.*;
 
 @Configuration
@@ -47,7 +49,11 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/login")
                 .permitAll()
-                .defaultSuccessUrl("/courses",true);
+                .defaultSuccessUrl("/courses",true)
+                .and()
+                .rememberMe()
+                .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21)).key("somethingVerySecure");
+
 
     }
 
